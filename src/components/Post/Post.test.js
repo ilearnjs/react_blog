8f263fb8e92a8886eafdf6034717f1768a936ab4
antonenko.p <antonenko.pvl@gmail.com>
2018-04-04 01:37:47 +0300

    Update snapshot tests with enzyme. Simulate and test remove click

--- src/components/Post/Post.test.js
+++ src/components/Post/Post.test.js
@@ -1,28 +1,62 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router';
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 
 import Post from './Post';
 
-it('renders correctly', () => {
-	const post = {
-		user: {
-			name: 'Jurgen',
-		},
-		createdOn: new Date('01-01-2018'),
-		content: 'Azaza'
-	};
+const post = {
+	_id: 332,
+	user: {
+		name: 'Alisa',
+	},
+	createdOn: new Date('01-01-2018'),
+	content: 'Azaza'
+};
 
-	const user = {
+const creator = {
+	name: 'Alisa',
+};
 
-	};
+const notCreator = {
+	name: 'Bob',
+};
 
-	const renderedPost = renderer
-		.create(
-			<MemoryRouter>
-				<Post post={post} currentUser={user} />
-			</MemoryRouter>
-		)
-		.toJSON();
-	expect(renderedPost).toMatchSnapshot();
+describe('Post.jsx', () => {
+	test('renders correctly for creator', () => {
+		const wrapper = shallow(
+			<Post post={post} currentUser={creator} />
+		);
+		expect(toJSON(wrapper)).toMatchSnapshot();
+	});
+
+	test('renders correctly for not creator', () => {
+		const wrapper = shallow(
+			<Post post={post} currentUser={notCreator} />
+		);
+		expect(toJSON(wrapper)).toMatchSnapshot();
+	});
+
+	test('remove post called', () => {
+		const removePost = jest.fn();
+
+		const wrapper = shallow(
+			<Post post={post} currentUser={creator} removePost={removePost} />
+		);
+
+		wrapper.find(".remove-btn").simulate("click");
+		expect(removePost.mock.calls.length).toBe(1);
+	});
+
+	test('remove post called with correct id', () => {
+		const removePost = jest.fn();
+
+		const wrapper = shallow(
+			<Post post={post} currentUser={creator} removePost={removePost} />
+		);
+
+		wrapper.find(".remove-btn").simulate("click");
+		expect(removePost.mock.calls[0][0]).toBe(post._id);
+	});
 });
